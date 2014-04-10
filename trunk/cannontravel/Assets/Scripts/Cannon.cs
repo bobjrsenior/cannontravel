da01dec883d8398f827585307b102d627f0904e5
bobjrsenior <bobjrsenior@8cc638e9-eea6-64fe-b2a7-2cea966429aa>
2014-04-10 21:00:58 +0000

    Added Player and Cannon Began Scripts for them/framework

---
+++ trunk/cannontravel/Assets/Scripts/Cannon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+
+	public bool has_player;
+	private int dir;
+	private float rotate_speed;
+
+	// Use this for initialization
+	void Start () {
+		has_player = false;
+		dir = (int) Mathf.Round(Random.Range (0, 1));
+		rotate_speed = Random.Range (20, 50);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (has_player) {
+			if(dir == 1){
+				transform.Rotate(0, 0, rotate_speed * Time.deltaTime);
+			}
+			else{
+				transform.Rotate(0, 0, -rotate_speed * Time.deltaTime);
+			}
+			if(Input.GetButtonDown("Jump")){
+
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Player")) {
+			has_player = true;
+		}
+	}
+}
---
+++ trunk/cannontravel/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public bool in_barrel;
+
+	// Use this for initialization
+	void Start () {
+		in_barrel = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Barrel")) {
+			in_barrel = true;
+		}
+	}
+}
