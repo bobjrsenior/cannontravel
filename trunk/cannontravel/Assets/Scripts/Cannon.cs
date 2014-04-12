0bdf7419139c67ae4c85cebb5773ba61d9c3e64f
bobjrsenior <bobjrsenior@8cc638e9-eea6-64fe-b2a7-2cea966429aa>
2014-04-12 19:04:51 +0000

    Updated ignore file.  Added sprites.

--- trunk/cannontravel/Assets/Scripts/Cannon.cs
+++ trunk/cannontravel/Assets/Scripts/Cannon.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class Cannon : MonoBehaviour {
 
 	public bool has_player;
+	private GameObject player;
 	private int dir;
 	private float rotate_speed;
+	private float strength;
 
 	// Use this for initialization
 	void Start () {
 		has_player = false;
 		dir = (int) Mathf.Round(Random.Range (0, 1));
 		rotate_speed = Random.Range (20, 50);
+		strength = 100;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 		if (has_player) {
 			if(dir == 1){
 				transform.Rotate(0, 0, rotate_speed * Time.deltaTime);
@@ -24,14 +27,17 @@ public class Cannon : MonoBehaviour {
 				transform.Rotate(0, 0, -rotate_speed * Time.deltaTime);
 			}
 			if(Input.GetButtonDown("Jump")){
-
+				has_player = false;
+				player.SendMessage("Fire", strength);
 			}
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.CompareTag ("Player")) {
+			Debug.Log("t2");
 			has_player = true;
+			player = other.gameObject;
 		}
 	}
 }
--- trunk/cannontravel/Assets/Scripts/Player.cs
+++ trunk/cannontravel/Assets/Scripts/Player.cs
@@ -4,20 +4,56 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public bool in_barrel;
+	private GameObject cannon;
+	private bool barrel_fix;
 
 	// Use this for initialization
 	void Start () {
-		in_barrel = true;
+		in_barrel = false;
+		barrel_fix = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (in_barrel) {
+			transform.position = cannon.transform.position;
+			transform.rotation = cannon.transform.rotation;
+			transform.Rotate(0, 0, -90);
+			rigidbody2D.velocity = new Vector2(0, 0);
+		}
+		else if(barrel_fix == true && Vector2.Distance(gameObject.transform.position, cannon.transform.position) > 2){
+			barrel_fix = false;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.CompareTag ("Barrel")) {
+		if (!barrel_fix && other.CompareTag ("Barrel")) {
+			Debug.Log("t1");
 			in_barrel = true;
+			barrel_fix = true;
+			cannon = other.gameObject;
+			//rigidbody2D.isKinematic = true;
+			rigidbody2D.gravityScale = 0;
+			transform.position = cannon.transform.position;
+			transform.rotation = cannon.transform.rotation;
 		}
+	}
+
+
+
+	/*void OnTriggerExit2D(Collider2D other) {
+		if (other.CompareTag ("Barrel")) {
+			in_barrel = false;
+			//rigidbody2D.isKinematic = true;
+		}
+	}*/
+
+	void Fire(float strength){
+		//rigidbody2D.isKinematic = false;
+		rigidbody2D.gravityScale = 1;
+		float t1 = Mathf.Sin(transform.rotation.z + 90);
+		float t2 = Mathf.Cos(transform.rotation.z + 90);
+		rigidbody2D.AddForce(new Vector2(strength * t2, strength * t1));
+		in_barrel = false;
 	}
 }
